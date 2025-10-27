c5db2070064ced07a2ed8a527f0e1aae673a155d
jay-awankar <jatinawankar02@gmail.com>
2025-10-27 20:05:15 +0530

    fix: PreferencesSection fields updated

--- app/post-a-ride/page.tsx
+++ app/post-a-ride/page.tsx
@@ -221,7 +221,7 @@ const PostRide = () => {
           totalSteps={steps.length}
           steps={steps}
         />
-        <div className="max-w-4xl mx-auto p-4 space-y-6">
+        <div className="max-w-4xl mx-auto p-4 pt-6 space-y-6">
           {renderCurrentStep()}
 
           {/* Navigation */}
@@ -280,9 +280,9 @@ const PostRide = () => {
                 </p>
               </div>
             </div>
-            <div className="flex flex-col sm:flex-row gap-2 items-end space-x-2">
+            <div className="flex flex-col sm:flex-row gap-2 w-fit items-end ">
               <Button
-                variant="ghost"
+                variant="destructive"
                 size="sm"
                 onClick={() => {
                   if (confirm("Are you sure you want to clear all data?")) {
@@ -290,15 +290,17 @@ const PostRide = () => {
                     window.location.reload();
                   }
                 }}
+                className=""
               >
                 <Trash2 />
                 Clear All
               </Button>
 
               <Button
-                variant="outline"
+                variant="ghost"
                 size="sm"
                 onClick={() => router.push("/dashboard")}
+                className="bg-accent dark:bg-accent/50"
               >
                 <Home />
                 Save & Exit
